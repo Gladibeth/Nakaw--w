9d21f65d8b0b9bfb5a94d83ec9afb92f0d537cb5
Gladibeth <gladibethdelos@gmail>
2019-10-15 16:48:37 -0400

    Add views details , all product

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -34,3 +34,20 @@ $('.main-banner__content').slick({
     }
   ]
 });
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  arrows: true,
+  centerMode: true,
+  focusOnSelect: true
+});
