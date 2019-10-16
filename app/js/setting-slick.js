133380c0908cd6ba547d0e9937c5b814f954e082
Gladibeth <gladibethdelos@gmail>
2019-10-16 16:58:37 -0400

    Deails responsive

--- app/js/setting-slick.js
+++ app/js/setting-slick.js
@@ -49,5 +49,33 @@ $('.slider-nav').slick({
   dots: true,
   arrows: true,
   centerMode: true,
-  focusOnSelect: true
+  focusOnSelect: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
 });
